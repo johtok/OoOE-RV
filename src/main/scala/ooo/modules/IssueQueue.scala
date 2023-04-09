7ce70135b5a84f2a2f84b87911ba6c4428049536
McHerman <karl@krauses.dk>
2023-04-10 00:18:40 +0200

    Rewrote issuequeue to be compatible with shared bundles, small changes. not tested

--- src/main/scala/ooo/modules/IssueQueue.scala
+++ src/main/scala/ooo/modules/IssueQueue.scala
@@ -3,125 +3,63 @@ package ooo.modules
 import chisel3._
 import chisel3.internal.firrtl.Width
 //import chisel3.util.{Fill, log2Ceil, MixedVec}
+import ooo.Types._
+import ooo.Configuration
+//import chisel3.util.{Decoupled, MuxCase, Valid}
 import chisel3.util._
-import chisel3.util.Decoupled
-import ooo.modules.IssueQueue.{InstPort, IssuePort, ElementPort}
+import ooo.modules.IssueQueue.{ ElementPort}
 import ooo.util.BundleExpander
 
 object IssueQueue {
-
-  /*
-
-  class InstPort(tagWidth: Width) extends Bundle {
-    val rdTag = Input(UInt(tagWidth))
-    val rs1Tag = Input(UInt(tagWidth))
-    val rs2Tag = Input(UInt(tagWidth))
-    val OP = Input(UInt(6.W))
-    val Offset_PC = Input(UInt(32.W))
-  }
-
-  */
-
-
-  class InstPort(tagWidth: Width) extends Bundle {
-    val rdTag = Output(UInt(tagWidth))
-    val rs1Tag = Output(UInt(tagWidth))
-    val rs2Tag = Output(UInt(tagWidth))
-    val OP = Output(UInt(6.W))
-    val Offset_PC = Output(UInt(32.W))
-  }
-
-  class IssuePort(tagWidth: Width) extends Bundle {
-    val rdTag = Output(UInt(tagWidth))
-    val rs1 = Output(UInt(32.W))
-    val rs2 = Output(UInt(32.W))
-    val OP = Output(UInt(6.W))
-    val Offset_PC = Output(UInt(32.W))
-  }
-
-  class ElementPort(tagWidth: Width) extends Bundle{
-    val In = Flipped(Decoupled(new InstPort(tagWidth)))
-    val Issue = Decoupled(new InstPort(tagWidth))
+  class ElementPort(implicit c: Configuration) extends Bundle{
+    val In = Flipped(Decoupled(new IssuePackage))
+    val Issue = Decoupled(new IssuePackage)
     val IssueReady = Output(Bool())
     val Age = Output(UInt(5.W))
-    val EventTag = Input(UInt(tagWidth))
-    val Event = Input(Bool())
-
-    //val reset = Input(Bool())
+    val eventBus = Flipped(Valid(new Event))
   }
 
   def apply(tagCount: Int): IdAllocator = Module(new IdAllocator(tagCount))
 
 }
-class IssueQueue(tagCount: Int, positions: Int) extends Module {
-
-  val w = log2Ceil(tagCount - 1).W
-  def Id() = UInt(w)
+class IssueQueue()(implicit c: Configuration) extends Module {
+  val positions = c.issueQueueSize
+  val w = c.physRegisterIdWidth
 
   val PosIndex = log2Ceil(positions - 1).W
 
   val io = IO(new Bundle {
-    val Alloc = Flipped(Decoupled(new InstPort(w)))
-    val Issue = Decoupled(new InstPort(w))
-    val EventTag = Input(UInt(w))
-    val Event = Input(Bool())
+    val Alloc = Flipped(Decoupled(new IssuePackage))
+    val Issue = Decoupled(new IssuePackage)
 
+    val eventBus = Flipped(Valid(new Event))
   })
 
-  io.Issue.bits.rdTag := 0.U
-  io.Issue.bits.rs1Tag := 0.U
-  io.Issue.bits.rs2Tag := 0.U
-  io.Issue.bits.OP := 0.U
-  io.Issue.bits.Offset_PC := 0.U
 
   io.Issue.valid := false.B
+  io.Alloc.ready := false.B
 
   // Instansiate issue queue
 
-  val QueueVec = Wire(Vec(positions, new ElementPort(w)))
+  val QueueVec = Wire(Vec(positions, new ElementPort))
 
   for(i <- 0 until positions){
-    val Element = Module(new IssueElement(w))
-    //Element.io <> QueueVec(i)
-    Element.io.Port <> QueueVec(i)
-    //Element.io.Issue <> QueueVec(i).Issue
+    val Element = Module(new IssueElement())
 
-    QueueVec(i).In.bits.rdTag := 0.U
-    QueueVec(i).In.bits.rs1Tag := 0.U
-    QueueVec(i).In.bits.rs2Tag := 0.U
-    QueueVec(i).In.bits.OP := 0.U
-    QueueVec(i).In.bits.Offset_PC := 0.U
+    QueueVec(i) <> Element.io.Port
+
+    QueueVec(i).eventBus <> io.eventBus
+
+    QueueVec(i).In.bits <> io.Alloc.bits
+    QueueVec(i).Issue.bits <> io.Issue.bits
 
     QueueVec(i).In.valid := false.B
-
     QueueVec(i).Issue.ready := false.B
-
-    QueueVec(i).EventTag := io.EventTag
-    QueueVec(i).Event := io.Event
-
   }
 
-  // Check wether queue is full 
+  // Checks if any positions are available 
 
-  val Carry = Wire(Vec(positions + 1, Bool()))
-  Carry(0) := false.B
-
-  //val Full = Wire(Bool())
-
-  
-  for(i <- 0 until positions){ // Checks if Queue is full 
-    //Carry(i+1) := Mux(Carry(i),true.B,QueueVec(i).In.ready)
-
-    when(Carry(i)){
-      Carry(i+1) := true.B
-    }.otherwise{
-      Carry(i+1) := QueueVec(i).In.ready
-    }
-  }
-
-  //Full := !Carry(positions) 
-
-  io.Alloc.ready := Carry(positions)
+  io.Alloc.ready := VecInit(Seq.tabulate(positions)(n => QueueVec(n).In.ready)).reduceTree(_ | _)
 
   //Write instruction to queue
 
@@ -136,7 +74,7 @@ class IssueQueue(tagCount: Int, positions: Int) extends Module {
     for(i <- 0 until positions){
       when(!WriteCarry(i)){
         when(QueueVec(i).In.ready){
-          QueueVec(i).In.bits := io.Alloc.bits 
+          //QueueVec(i).In.bits := io.Alloc.bits 
           QueueVec(i).In.valid := true.B
           WriteCarry(i+1) := true.B
         }.otherwise{
@@ -147,15 +85,10 @@ class IssueQueue(tagCount: Int, positions: Int) extends Module {
       }
     }
   } 
+
   
-  val AgeVec = Wire(Vec(positions,UInt(5.W)))
 
-  for(i <- 0 until positions){
-    AgeVec(i) := QueueVec(i).Age
-  }
-
-
-  //AgeVec := QueueVec.Age
+  val AgeVec = VecInit(Seq.tabulate(positions)(n => QueueVec(n).Age))
 
   val scalaVector = AgeVec.zipWithIndex
   .map ((x) => MixedVecInit (x._1 , x._2.U(8.W)))
@@ -164,60 +97,51 @@ class IssueQueue(tagCount: Int, positions: Int) extends Module {
   val maxVal = resFun2 (0)
   val maxIdx = resFun2 (1)
 
+  io.Issue.bits <> QueueVec(maxIdx).Issue.bits
+
   when(io.Issue.ready && (maxVal > 0.U)){
     QueueVec(maxIdx).Issue.ready := true.B
-    io.Issue.bits := QueueVec(maxIdx).Issue.bits
     io.Issue.valid := true.B
   }
+
 }
 
-
-
-class IssueElement(tagWidth: Width) extends Module{
+class IssueElement()(implicit c: Configuration) extends Module{
   val io = IO(new Bundle {
-    val Port = new ElementPort(tagWidth)
+    val Port = new ElementPort()
   })
 
-  io.Port.IssueReady := false.B
-
-  io.Port.Issue.bits.rdTag := 0.U
-  io.Port.Issue.bits.rs1Tag := 0.U
-  io.Port.Issue.bits.rs2Tag := 0.U
-  io.Port.Issue.bits.OP := 0.U
-  io.Port.Issue.bits.Offset_PC := 0.U
   io.Port.Issue.valid := false.B
-
   io.Port.In.ready := false.B
 
+  io.Port.IssueReady := false.B
   io.Port.Age := 0.U
 
-  val valueReg = Reg(new InstPort(tagWidth))
+
+  val valueReg = Reg(new IssuePackage())
 
   val emptyReg = RegInit(1.B)
   val AgeReg = RegInit(0.U(8.W))
 
-  val rs1_ready = RegInit(0.B)
-  val rs2_ready = RegInit(0.B)
-
-  when(io.Port.Event && !emptyReg){
-    when(io.Port.EventTag === valueReg.rs1Tag){
-      rs1_ready := true.B
+  when(io.Port.eventBus.valid && !emptyReg){
+    when(io.Port.eventBus.bits.pr === valueReg.prs(0).id){
+      valueReg.prs(0).ready := true.B
     }
 
-    when(io.Port.EventTag === valueReg.rs2Tag){
-      rs2_ready := true.B
+    when(io.Port.eventBus.bits.pr === valueReg.prs(1).id){
+      valueReg.prs(1).ready := true.B
     }
   }
 
-  when(rs1_ready && rs2_ready){
+
+  when(valueReg.prs(0).ready && valueReg.prs(1).ready){
     io.Port.IssueReady := true.B
     io.Port.Age := AgeReg
 
     when(io.Port.Issue.ready){
       io.Port.Issue.valid := true.B
-      io.Port.Issue.bits := valueReg
-      rs1_ready := false.B
-      rs1_ready := false.B
+      valueReg.prs(0).ready := false.B
+      valueReg.prs(1).ready := false.B
 
       emptyReg := true.B
       AgeReg := 0.U
@@ -236,5 +160,8 @@ class IssueElement(tagWidth: Width) extends Module{
     }
   }
 
+  io.Port.Issue.bits := valueReg
 
 }
+
+object Penis extends App { emitVerilog(new IssueQueue()(Configuration.random()))}
