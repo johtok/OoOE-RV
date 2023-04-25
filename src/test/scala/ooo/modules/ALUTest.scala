6786d5d6f47e792426db405d13dce7b4290fbc4b
IIIV <simonbl@outlook.com>
2023-04-25 13:30:46 +0000

    Last tests

--- src/test/scala/ooo/modules/ALUTest.scala
+++ src/test/scala/ooo/modules/ALUTest.scala
@@ -45,6 +45,22 @@ class ALUTest extends AnyFlatSpec with ChiselScalatestTester {
         dut.io.out.output.expect(output)
     }
   }
+  it should "Shift left logical" in {
+    val func3 = 1
+    val func_mod = 0
+    val input1 = 81233
+    val input2 = 22
+    val output = input1 << input2
+    test(new ALU(XLEN)) { 
+      dut =>
+        dut.io.in.input1.poke(input1)
+        dut.io.in.input2.poke(input2)
+        dut.io.in.func3.poke(func3)
+        dut.io.in.func_mod.poke(func_mod)
+        dut.clock.step()
+        dut.io.out.output.expect(output)
+    }
+  }
   it should "Compare logical 2 values" in {
     val func3 = 2
     val func_mod = 0
@@ -83,6 +99,38 @@ class ALUTest extends AnyFlatSpec with ChiselScalatestTester {
     val input1 = 987234
     val input2 = Int.MaxValue - 9124L
     val output = input1 ^ input2
+    test(new ALU(XLEN)) { 
+      dut =>
+        dut.io.in.input1.poke(input1)
+        dut.io.in.input2.poke(input2)
+        dut.io.in.func3.poke(func3)
+        dut.io.in.func_mod.poke(func_mod)
+        dut.clock.step()
+        dut.io.out.output.expect(output)
+    }
+  }
+  it should "Shift right logical" in {
+    val func3 = 5
+    val func_mod = 0
+    val input1 = 81233
+    val input2 = 22
+    val output = input1 >> input2
+    test(new ALU(XLEN)) { 
+      dut =>
+        dut.io.in.input1.poke(input1)
+        dut.io.in.input2.poke(input2)
+        dut.io.in.func3.poke(func3)
+        dut.io.in.func_mod.poke(func_mod)
+        dut.clock.step()
+        dut.io.out.output.expect(output)
+    }
+  }
+  it should "Shift right arithmetic" in {
+    val func3 = 5
+    val func_mod = 1
+    val input1 = 81233
+    val input2 = 22
+    val output = input1 >> input2
     test(new ALU(XLEN)) { 
       dut =>
         dut.io.in.input1.poke(input1)
