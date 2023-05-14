18fea68f0424ce2cd0ebc0d354428e3001442bf6
McHerman <karl@krauses.dk>
2023-05-14 21:34:22 +0200

    fixed issuequeuetest

--- src/test/scala/ooo/modules/IssueQueueTest.scala
+++ src/test/scala/ooo/modules/IssueQueueTest.scala
@@ -3,6 +3,10 @@ package ooo.modules
 import chisel3._
 import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
+import ooo.Configuration
+import ooo.Types.EventType._
+
+
 
 import scala.util.Random
 import ooo.util.TestingUtils._
@@ -10,12 +14,16 @@ import ooo.util.TestingUtils._
 class IssueQueueTest extends AnyFlatSpec with ChiselScalatestTester {
 
   behavior of "IssueQueue"
-/*
+
+  val n = 2 + Random.nextInt(30)
+
+  implicit val Config = Configuration.default().copy(issueQueueSize = n, simulation = true)
+
   it should "allocate n spaces sequentially" in {
-    val n = Random.nextInt(2 until 32)
+    //val n = 2 + Random.nextInt(30)
     //val n = 16
 
-    test(new IssueQueue(n+2,n)) { dut =>
+    test(new IssueQueue()) { dut =>
       for(id <- 0 until n) {
 
         /*
@@ -24,11 +32,31 @@ class IssueQueueTest extends AnyFlatSpec with ChiselScalatestTester {
 
         */
         dut.io.Alloc.ready.expect(1.B)
-        dut.io.Alloc.bits.rdTag.poke(id.U)
-        dut.io.Alloc.bits.rs1Tag.poke(id.U)
-        dut.io.Alloc.bits.rs2Tag.poke((id+1).U)
-        dut.io.Alloc.bits.OP.poke(id.U)
-        dut.io.Alloc.bits.Offset_PC.poke(id.U)
+        dut.io.Alloc.bits.prd.poke(id.U)
+
+        dut.io.Alloc.bits.prs(0).ready.poke(false.B)
+        dut.io.Alloc.bits.prs(1).ready.poke(false.B)
+
+        dut.io.Alloc.bits.prs(0).id.poke(id.U)
+        dut.io.Alloc.bits.prs(1).id.poke((id+1).U)
+
+        /*
+
+        dut.io.Alloc.bits.opcode := 0.U
+        dut.io.Alloc.bits.func := 0.U
+
+
+        dut.io.Alloc.bits.immediate := 0.U
+        dut.io.Alloc.bits.pc := 0.U
+
+
+        dut.io.Alloc.bits.snapshotId := 0.U
+        dut.io.Alloc.bits.branchPrediction := false.B
+
+        //dut.io.Alloc.bits.OP.poke(id.U)
+        //dut.io.Alloc.bits.Offset_PC.poke(id.U)
+
+        */
 
         dut.io.Alloc.valid.poke(1.B)
         
@@ -39,13 +67,11 @@ class IssueQueueTest extends AnyFlatSpec with ChiselScalatestTester {
     }
   }
 
+  
+
   it should "deallocate n spaces sequentially" in {
-    val n = Random.nextInt(2 until 64)
-    //val n = 16
 
-    println(n)
-
-    test(new IssueQueue(n+2,n)) { dut =>
+    test(new IssueQueue()) { dut =>
       for(id <- 0 until n) {
 
         /*
@@ -54,11 +80,13 @@ class IssueQueueTest extends AnyFlatSpec with ChiselScalatestTester {
 
         */
         dut.io.Alloc.ready.expect(1.B)
-        dut.io.Alloc.bits.rdTag.poke(id.U)
-        dut.io.Alloc.bits.rs1Tag.poke(id.U)
-        dut.io.Alloc.bits.rs2Tag.poke((id+1).U)
-        dut.io.Alloc.bits.OP.poke(id.U)
-        dut.io.Alloc.bits.Offset_PC.poke(id.U)
+        dut.io.Alloc.bits.prd.poke(id.U)
+
+        dut.io.Alloc.bits.prs(0).ready.poke(false.B)
+        dut.io.Alloc.bits.prs(1).ready.poke(false.B)
+
+        dut.io.Alloc.bits.prs(0).id.poke(id.U)
+        dut.io.Alloc.bits.prs(1).id.poke((id+1).U)
 
         dut.io.Alloc.valid.poke(1.B)
         
@@ -69,22 +97,31 @@ class IssueQueueTest extends AnyFlatSpec with ChiselScalatestTester {
       dut.io.Alloc.valid.poke(0.B)
       dut.io.Alloc.ready.expect(0.B)
 
-      dut.io.Event.poke(true.B)
-      dut.io.EventTag.poke(0.U)
+      //dut.io.Event.poke(true.B)
+      //dut.io.EventTag.poke(0.U)
+
+      dut.io.event.valid.poke(true.B)
+      dut.io.event.bits.eventType.poke(CompletionWithValue)
+      dut.io.event.bits.pr.poke(0.U)
+      dut.io.event.bits.writeBackValue.poke(0.U)
+
       dut.io.Issue.ready.poke(true.B)
       dut.io.Issue.valid.expect(false.B)
 
-      println("1")
+      //println("1")
 
 
       dut.clock.step()
 
-      dut.io.Event.poke(true.B)
-      dut.io.EventTag.poke(1.U)
+      dut.io.event.valid.poke(true.B)
+      dut.io.event.bits.eventType.poke(CompletionWithValue)
+      dut.io.event.bits.pr.poke(1.U)
+      dut.io.event.bits.writeBackValue.poke(0.U)
+
       dut.io.Issue.ready.poke(true.B)
       dut.io.Issue.valid.expect(false.B)
 
-      println("2")
+      //println("2")
 
 
       dut.clock.step()
@@ -92,12 +129,23 @@ class IssueQueueTest extends AnyFlatSpec with ChiselScalatestTester {
 
       for(tag <- 0 until n){
 
+        /*
+
         dut.io.Event.poke(true.B)
         dut.io.EventTag.poke((tag + 2).U)
+
+        */
+
+        dut.io.event.valid.poke(true.B)
+        dut.io.event.bits.eventType.poke(CompletionWithValue)
+        dut.io.event.bits.pr.poke((tag+2).U)
+        dut.io.event.bits.writeBackValue.poke(0.U)
+
+
         dut.io.Issue.ready.poke(true.B)
         dut.io.Issue.valid.expect(true.B)
 
-        dut.io.Issue.bits.rdTag.expect(tag.U)
+        dut.io.Issue.bits.prd.expect(tag.U)
 
         dut.clock.step()
       }
@@ -109,8 +157,12 @@ class IssueQueueTest extends AnyFlatSpec with ChiselScalatestTester {
 
   }
 
+
+
+  /*
+
   it should "release all n ids" in {
-    val n = Random.nextInt(2 until 64)
+    val n = 2 + Random.nextInt(62)
     test(new IdAllocator(n)) { dut =>
       dut.io.alloc.take.poke(1.B)
       dut.clock.step(n)
@@ -161,4 +213,6 @@ class IssueQueueTest extends AnyFlatSpec with ChiselScalatestTester {
   }
 
   */
+
+  
 }
